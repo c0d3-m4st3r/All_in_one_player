54999dbf241242ebf51352ff5478abf6543bf842
Francisco Ramos Vega <frv00009@red.ujaen.es>
2020-06-22 18:21:25 +0200

    Json y estructura de la app

---
+++ app/js/app.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    getCanciones();
+});
+
+var audio;
+var musica;
+
+function getCanciones(){
+    $.getJSON("js/app.json", function(mjson){
+        musica = mjson;
+        console.log(music);
+    });
+}
