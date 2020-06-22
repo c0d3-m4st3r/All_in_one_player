b530b01ccb1f82f9510d13332cb488a40ae4b05e
Francisco Javier Pedrajas Arroyo <fjpa0008@red.ujaen.es>
2020-06-22 19:51:28 +0200

    lista canciones

--- app/js/app.js
+++ app/js/app.js
@@ -9,5 +9,24 @@ function getCanciones(){
     $.getJSON("js/app.json", function(mjson){
         musica = mjson;
         console.log(musica);
+        getLista(musica);
     });
+}
+
+function getLista(musica){
+ console.log(musica.canciones);
+
+ $.each(musica.canciones,function(i,canciones){
+
+        $('#playlist').append('<li class="list-group-item" id="' +i+'">'+canciones.nombre+'</li>');
+
+});
+
+$('#playlist li').click(function(){
+
+var seleccionarCancion = $(this).attr('id');
+console.log(seleccionarCancion);
+
+});
+
 }
