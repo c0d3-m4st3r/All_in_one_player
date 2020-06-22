fa398bb7cd5ae6bf11108b4f0141a808024ca9f6
Francisco Ramos Vega <frv00009@red.ujaen.es>
2020-06-22 19:54:08 +0200

    lista y reproduccion working

--- app/js/app.js
+++ app/js/app.js
@@ -2,31 +2,30 @@ $(document).ready(function(){
     getCanciones();
 });
 
-var audio;
+var audio = document.getElementById('player');
 var musica;
 
 function getCanciones(){
     $.getJSON("js/app.json", function(mjson){
         musica = mjson;
-        console.log(musica);
         getLista(musica);
     });
 }
 
+function playCancion(id){
+    console.log(id);
+    $('#img-album').attr('src', musica.canciones[id].rutaImagen);
+    $('#player').attr('src', musica.canciones[id].rutaAudio);
+    audio.play();
+}
+
 function getLista(musica){
- console.log(musica.canciones);
-
- $.each(musica.canciones,function(i,canciones){
-
-        $('#playlist').append('<li class="list-group-item" id="' +i+'">'+canciones.nombre+'</li>');
-
-});
-
-$('#playlist li').click(function(){
-
-var seleccionarCancion = $(this).attr('id');
-console.log(seleccionarCancion);
-
-});
-
+    console.log(musica.canciones);
+    $.each(musica.canciones, function(i, cancion){
+        $('#playlist').append('<li class="list-group-item" id="' + i + '">' + cancion.nombre + '</li>');   
+    });
+    $('#playlist li').click(function(){
+        var cancionSeleccionada = $(this).attr('id');
+        playCancion(cancionSeleccionada);
+    })
 }
