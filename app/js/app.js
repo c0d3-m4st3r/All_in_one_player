ba442ec087a60722d553c6d53b9f62eaef490cb8
Francisco Ramos Vega <frv00009@red.ujaen.es>
2020-06-23 03:14:38 +0200

    playlist medio implementar

--- app/js/app.js
+++ app/js/app.js
@@ -1,6 +1,11 @@
 $(document).ready(function(){
-    getCanciones();
+    getInfo();
     iniciarReproductor();
+    $('#listasDisponibles li').click(function(){
+        var listaSeleccionada = $(this).attr('id');
+        $('#playlist').empty();
+        getCanciones(listaSeleccionada);
+    })
 });
 
 var audio = document.getElementById('player');
@@ -10,15 +15,20 @@ function iniciarReproductor(){
     $('#shuffle').click(function(){
         $('#playlist').empty();
         console.log(shuffle(musica.canciones));
-        getLista(musica);
+        $.each(musica.canciones, function(i, cancion){
+            $('#playlist').append('<li class="list-group-item" style="background-color: #696969; color: white;" id="' + i + '">' + cancion.nombre + '</li>');
+        });
         playCancion(0);
+    })
+    $('#nuevaPlaylist').click(function(){
+        $('#playlist').addClass();
     })
 }
 
-function getCanciones(){
+function getInfo(){
     $.getJSON("js/app.json", function(mjson){
         musica = mjson;
-        getLista(musica);
+        getCanciones(-1);
     });
 }
 
@@ -35,20 +45,33 @@ function playCancion(id){
     console.log("Hay más canciones");
     scheduleCancion(id);
 
-}
+    }
     
 }
 
-function getLista(musica){
-    console.log(musica.canciones);
-    $.each(musica.canciones, function(i, cancion){
-        $('#playlist').append('<li class="list-group-item" id="' + i + '">' + cancion.nombre + '</li>');   
+function getCanciones(idPlaylist){
+    var listaElegida = musica.listas[0];
+    $.each(musica.listas, function(lista){
+        if (lista.id == idPlaylist){
+            listaElegida = lista;
+        }
     });
+    $("#tituloPlaylist").text(listaElegida.nombre);
+    $.each(listaElegida.canciones, function(lista){
+        $.each(musica.canciones, function(i, cancion){
+            if(musica.canciones[i].id == lista){
+                $('#playlist').append('<li class="list-group-item" style="background-color: #696969; color: white;" id="' + i + '">' + cancion.nombre + '</li>');   
+            } 
+        });
+    });
+
     $('#playlist li').click(function(){
         var cancionSeleccionada = $(this).attr('id');
         playCancion(cancionSeleccionada);
     })
 }
+
+
 
 
 function scheduleCancion(id){
@@ -62,12 +85,3 @@ function shuffle(array){
     for(var random, temp, position = array.length; position; random = Math.floor(Math.random()*position), temp = array[--position], array[position] = array[random], array[random] = temp);
     return array;
 }
-
-
-
-
-
-
-
-
-
