aeef9d211bdfa6ec53718667904bd9db0a75d124
Francisco Javier Pedrajas Arroyo <fjpa0008@red.ujaen.es>
2020-06-23 21:03:05 +0200

    video

---
+++ app/js/video.js
@@ -0,0 +1,80 @@
+$(document).ready(function(){
+    
+
+getVideos();
+iniciarReproductor();
+
+
+
+});
+
+
+
+var video = document.getElementById('video');
+var archivos;
+
+
+function iniciarReproductor(){
+    $('#shuf').click(function(){
+        $('#playVideos').empty();
+        console.log(shuffle(archivos.videos));
+        getLista(archivos);
+        playVideo(0);
+    })
+}
+
+function getVideos(){
+    $.getJSON("js/video.json", function(mjson){
+        archivos = mjson;
+        console.log(mjson);
+        getLista(archivos);
+    });
+}
+
+function playVideo(id){
+    console.log(id);
+    var long = archivos.videos;
+    if(id >= long.length){
+       console.log("se acabó");
+       video.pause();
+    }else{
+    $('#video').attr('src', archivos.videos[id].rutaVideo);
+    video.play();
+    console.log("Hay más videos");
+    scheduleVideo(id);
+}
+
+}
+
+function getLista(videos){
+    console.log(videos.videos);
+    $.each(videos.videos, function(i, video){
+        $('#playVideos').append('<li class="list-group-item" id="' + i + '">' + video.nombre + '</li>');
+        
+              
+    });
+    $('#playVideos li').click(function(){
+        var videoSeleccionado = $(this).attr('id');
+        playVideo(videoSeleccionado);
+   
+    })
+}
+
+
+function scheduleVideo(id){
+    video.onended = function(){
+        console.log("termino el video");
+        playVideo(parseInt(id)+1);
+    }
+}
+
+function shuffle(array){
+    for(var random, temp, position = array.length; position; random = Math.floor(Math.random()*position), temp = array[--position], array[position] = array[random], array[random] = temp);
+    return array;
+}
+
+
+
+
+
+
